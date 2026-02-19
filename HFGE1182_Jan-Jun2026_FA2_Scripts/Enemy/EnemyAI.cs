dfe2b7337d2510bf22f634dea04d297c7b9a1a6d
Educator-Connor <Connor.Munro@boston.co.za>
2026-02-19 09:50:44 +0200

    Add files via upload

---
+++ HFGE1182_Jan-Jun2026_FA2_Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public enum EnemyState { Idle, Patrol, Chase, Attack, Dead }
+    [SerializeField] private EnemyState currentState = EnemyState.Idle;
+    
+    [Header("AI PARAMETERS")]
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    public float detectionRange = 10f;
+    public float attackRange = 6f;
+    public float patrolRange = 4f;
+
+    [Header("ATTACK PARAMETERS")] 
+    public GameObject projectilePrefab;
+    public Transform projectileSpawn;
+    public Transform playerTransform;
+
+    [Header("AREA RESTRICTION")] 
+    public Transform areaCenter;
+    public float areaRadius = 12f;
+    
+    private Vector3 target;
+    
+    [Header("ANIMATION")]
+    public Animator animator;
+    public HealthHandler healthHandler;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        navMeshAgent =  GetComponent<NavMeshAgent>();
+        PickNewPatrolPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentState == EnemyState.Dead)
+        {
+            return;
+        }
+        
+        float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
+
+        if (Vector3.Distance(transform.position, areaCenter.position) > areaRadius)
+        {
+            navMeshAgent.SetDestination(areaCenter.position);
+            
+            return;
+        }
+
+        if (healthHandler.GetHealth() <= 0)
+        {
+            currentState = EnemyState.Dead;
+            StateHandler();
+            return;
+        }
+
+        if (distanceToPlayer <= attackRange)
+        {
+            currentState = EnemyState.Attack;
+        }
+        else if (distanceToPlayer <= detectionRange)
+        {
+            currentState = EnemyState.Chase;
+        }
+        else
+        {
+            currentState = EnemyState.Patrol;
+        }
+        
+        StateHandler();
+    }
+
+    public void StateHandler()
+    {
+        //Debug.Log(enemyBehaviour);
+      
+        switch (currentState)
+        {
+            case EnemyState.Idle:
+                navMeshAgent.isStopped = false;
+                animator.SetBool("Attacking", false);
+                animator.SetBool("Running", false);
+                StartCoroutine(IdleFor(1f));
+                currentState = EnemyState.Patrol;
+                break;
+            case EnemyState.Patrol:
+                animator.SetBool("Attacking", false);
+                animator.SetBool("Running", true);
+                PatrolRoutine();
+                break;
+            case EnemyState.Chase:
+                navMeshAgent.isStopped = false;
+                animator.SetBool("Attacking", false);
+                animator.SetBool("Running", true);
+                navMeshAgent.SetDestination(playerTransform.position);
+                break;
+            case EnemyState.Attack:
+                navMeshAgent.isStopped = true;
+                animator.SetBool("Attacking", true);
+                animator.SetBool("Running", false);
+                transform.LookAt(playerTransform.position);
+                break;
+            case EnemyState.Dead:
+                
+                animator.SetTrigger("Dead");
+                break;
+        }
+    }
+    
+    //PATROL
+
+    public void PatrolRoutine()
+    {
+        navMeshAgent.isStopped = false;
+
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < .5f)
+        {
+            PickNewPatrolPoint();
+        }
+    }
+
+    public void PickNewPatrolPoint()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * patrolRange;
+        randomDirection += areaCenter.position;
+        
+        NavMeshHit hit;
+
+        if (NavMesh.SamplePosition(randomDirection, out hit, patrolRange, NavMesh.AllAreas))
+        {
+            target = hit.position;
+            navMeshAgent.SetDestination(target);
+        }
+    }
+
+    public IEnumerator IdleFor(float time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+    
+    //ATTACK
+
+    //Hint: shifted to animation event driven code
+    public void FireProjectile()
+    {
+        if (projectilePrefab != null && projectileSpawn != null)
+        {
+            Instantiate(projectilePrefab, projectileSpawn.position, projectileSpawn.rotation);
+        }
+    }
+    
+}
---
+++ HFGE1182_Jan-Jun2026_FA2_Scripts/Player/ShootHandler.cs
@@ -0,0 +1,133 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+
+public class ShootHandler : MonoBehaviour
+{
+    [Header("ACTIONS")]
+    public PlayerInputActions InputAction;
+    
+    [Header("PARAMETERS")]
+    [SerializeField] private float maxDistance;
+    [SerializeField] private float damage;
+    [SerializeField] private float spread;
+    [SerializeField] private int projectiles;
+    [SerializeField] private LayerMask layerMask;
+    
+    [Header("AMMO")]
+    [SerializeField] private int currentAmmo;
+    [SerializeField] private int maxAmmo;
+    [SerializeField] private int totalAmmo;
+
+    private Camera cam;
+    
+    [Header("ANIMATION")]
+    [SerializeField] private Animator animatorHands;
+    [SerializeField] private Animator animatorGun;
+    
+    [Header("PARTICLE")]
+    [SerializeField] private GameObject particlePrefab;
+    [SerializeField] private float particleDuration;
+    
+    
+    private void Awake()
+    {
+        InputAction = new PlayerInputActions();
+        cam = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        InputAction.Player.Enable();
+        InputAction.Player.Attack.performed += OnShoot;
+        InputAction.Player.Reload.performed += OnReload;
+    }
+
+    private void OnDisable()
+    {
+        InputAction.Player.Attack.performed -= OnShoot;
+        InputAction.Player.Reload.performed -= OnReload;
+        InputAction.Player.Disable();
+    }
+
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        if (currentAmmo <= 0)
+        {
+            OnReload(new InputAction.CallbackContext());
+            return;
+        }
+        
+        animatorGun.SetTrigger("Shoot");
+        animatorHands.SetTrigger("Shoot");
+        currentAmmo--;
+        RaycastHit hit;
+        for (int i = 0; i < projectiles; i++)
+        {
+            Vector3 dir = cam.transform.forward;
+            Vector3 spreadOffset = Vector3.zero;
+            spreadOffset += cam.transform.up * Random.Range(-spread, spread);
+            spreadOffset += cam.transform.right * Random.Range(-spread, spread);
+            dir += (dir + spreadOffset).normalized;
+            
+            if (Physics.Raycast(cam.transform.position, dir, out hit, maxDistance, layerMask))
+            {
+                if (hit.collider.GetComponent<HealthHandler>())
+                {
+                    hit.collider.gameObject.GetComponent<HealthHandler>().DamageHandler("Player", damage);
+                    if (particlePrefab == null)
+                    {
+                        return;
+                    }
+                    
+                    GameObject hitParticle = Instantiate(particlePrefab, hit.transform.position, Quaternion.identity);
+                    Destroy(hitParticle, particleDuration);
+                }
+            }
+        }
+    }
+    
+    private void OnReload(InputAction.CallbackContext context)
+    {
+        animatorGun.SetTrigger("Reload");
+        animatorHands.SetTrigger("Reload");
+        
+        if (totalAmmo < maxAmmo)
+        {
+            currentAmmo = totalAmmo;
+            totalAmmo = 0;
+        }
+        else
+        {
+            totalAmmo -= maxAmmo;
+            currentAmmo = maxAmmo;
+        }
+    }
+    
+    public void AddAmmo(int amount)
+    {
+        totalAmmo += amount;
+    }
+
+    public int GetCurrentAmmo()
+    {
+        return currentAmmo;
+    }
+    
+    public int GetTotalAmmo()
+    {
+        return totalAmmo;
+    }
+
+    public void SetCurrentAmmo(int amount)
+    {
+        currentAmmo = amount;
+    }
+    public void SetTotalAmmo(int amount)
+    {
+        totalAmmo = amount;
+    }
+}
